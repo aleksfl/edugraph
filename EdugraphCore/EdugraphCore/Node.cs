9fb059f59b1d01308502473de07a9c2347d64e37
Aleksander <sasja_git@bef.no>
2023-01-29 12:20:02 +0100

    Make initial datastructures

---
+++ EdugraphCore/EdugraphCore/Node.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace EdugraphCore {
+
+    public enum NodeType {
+        Subject,
+        Knowledge
+
+    }
+
+    public class Node {
+
+        // Either automatically set to an int, or to a subject code manually.
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public NodeType Type { get; set; }
+
+        // Check if id is a subject code, which implies that the node represents a subject.
+        public NodeType CheckType { get {
+                if (Id.Any(a => char.IsLetter(a))) {
+                    return NodeType.Subject;
+                } else {
+                    return NodeType.Knowledge;
+                }
+            } }
+    }
+}
---
+++ EdugraphCore/EdugraphCore/Relation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace EdugraphCore {
+
+    public enum DependancyType {
+        DependantOn,
+        HelpedBy,
+        PartOf,
+
+    }
+
+    // Can represent a direct dependancy between nodes, or part of a multi node chain.
+    public class Dependancy {
+        public string DependantNodeId { get; set; }
+        public string ProviderNodeId { get; set; }
+        public DependancyType Type { get; set; }
+        public string DependancyObjectId { get; set; }
+        // Is this a direct dependancy or part of a multi node chain?
+        public bool IsDirectDependancy { get {
+                return ProviderNodeId == DependancyObjectId;
+            }            
+        }
+        
+    }
+}
